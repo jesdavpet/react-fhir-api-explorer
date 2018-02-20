df46e073040b7d6b572744573daa976062cd8439
Jesse David Peterson <jesdavpet@gmail.com>
2018-02-19 21:52:50 -0500

    Eliminated duplicate lists by including labels in routes.

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -3,21 +3,16 @@ import {Glyphicon, Nav, Navbar, NavItem} from 'react-bootstrap'
 import {NavLink} from 'redux-first-router-link'
 import {push} from 'redux-first-router'
 
-import {HOME, AGGREGATOR, EXPLORER} from '../routes'
+import {HOME, EXPLORER, AGGREGATOR} from '../routes'
 
-const homeLink = {path: HOME, label: `HL7/FHIR Crash Course`, glyph: `fire`}
-const explorerLink = {path: EXPLORER, label: `Explore a FHIR API`}
-const aggregatorLink =  {path: AGGREGATOR, label: `Visualize FHIR Data`}
-
-const sectionLinks = [explorerLink, aggregatorLink]
-
+const navLinks = [EXPLORER, AGGREGATOR]
 export default class Navigation extends Component {
   render () {
     return <Navbar inverse collapseOnSelect>
       <Navbar.Header>
         <Navbar.Brand>
-          <NavLink to={homeLink.path}>
-            <Glyphicon glyph={homeLink.glyph} />{` `}{homeLink.label}
+          <NavLink to={HOME.path}>
+            <Glyphicon glyph="fire" />{` `}{HOME.label}
           </NavLink>
         </Navbar.Brand>
 
@@ -26,7 +21,7 @@ export default class Navigation extends Component {
 
       <Navbar.Collapse>
         <Nav>
-        {sectionLinks.map((link, i) =>
+        {navLinks.map((link, i) =>
           <NavItem key={i} onClick={() => push(link.path)}>
             {link.label}
           </NavItem>
--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 
 import Root from './Root'
-import Navigation from './Navigation'
 import registerServiceWorker from './registerServiceWorker'
 import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
-    <div>
-      <Navigation />
-      <Root />
-    </div>
+    <Root />
   </Provider>,
   document.getElementById('root')
 )
