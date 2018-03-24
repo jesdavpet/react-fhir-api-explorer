66f38a9816b166bf6a0cf4d0e3fc7859cd62d14b
jesdavpet@github <jesdavpet@gmail.com>
2018-03-23 22:36:56 -0400

    WIP
    WIP

---
+++ TODO.md
@@ -0,0 +1,23 @@
+Routing
+-------
+  - Temporarily make `/explorer` default to skip to dev 
+
+Guided
+------
+  - Design a workflow for the guided tour demo
+  - Create a dataset for the steps of the guided tour
+    - How to integrate instructions / tooltips?
+    - How to handle enqueue/dequeue in reducer?
+
+Cover
+-----
+  - ~~Add a title and description~~
+  - Create a FHIR background graphic
+  - Add navigation links for the guided tour and explorer
+  - Generate a shortened URL, display prominently
+
+Design
+------
+  - Create a more compelling repsonsive layout
+  - Use a Bootstrap 3 theme generator to make graphics fancier
+  - Confirm that colour scheme will project well
--- src/Cover/Cover.js
+++ src/Cover/Cover.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react'
-import { Grid } from 'react-bootstrap'
+import {NavLink} from 'redux-first-router-link'
+import {
+  Button,
+  Col,
+  Grid,
+  Glyphicon,
+  Jumbotron,
+  Row
+} from 'react-bootstrap'
+
+import {EXPLORER, AGGREGATOR} from '../routes'
 
 class Cover extends Component {
   render() {
     return <Grid>
-      <h1>Hello World!</h1>
+      <Row>
+        <Col horizontal-align="right">
+          <h1>Intro to HL7/FHIR for Beginners</h1>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <Jumbotron>
+            <section>
+              <p>
+                Start with a <NavLink to={EXPLORER.path}>step-by-step tutorial</NavLink> guiding you through simple interactions with ReSTful FHIR APIs.
+                Then <NavLink to={AGGREGATOR.path}>visualize</NavLink> recently submitted FHIR data in an aggregate view.
+                Finally, when you&apos;re ready, <NavLink to={EXPLORER.path}>start exploring</NavLink> common FHIR APIs on your own.
+              </p>
+            </section>
+
+            <section>
+              <Button bsStyle="primary" bsSize="lg">
+                Let&apos;s Get Started{` `}<Glyphicon glyph="arrow-right" />
+              </Button>
+            </section>
+          </Jumbotron>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <h3><Glyphicon glyph="globe" />{` `}www.short.url/goes-here</h3>
+        </Col>
+      </Row>
     </Grid>
   }
 }
 
 export default Cover
+
